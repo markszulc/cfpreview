fe6786969ac8f20bc765c07628413698c03bc654
Mark Szulc <mszulc@adobe.com>
2024-07-10 12:48:30 +1000

    phone wrapper

--- src/dx-excshell-1/web-src/src/components/layouts/CFApp150x150.js
+++ src/dx-excshell-1/web-src/src/components/layouts/CFApp150x150.js
@@ -5,12 +5,14 @@ export default function CFBanner({ contentfragment, label}) {
 
   return (
     <div>
-    <div className='app-150x150-content'>
-      <div className='banner-text'>
-        <h1>{contentfragment.headline}</h1>
+      <div className='phone-wrapper'>
+        <div className='app-150x150-content'>
+          <div className='banner-text'>
+            <h1>{contentfragment.headline}</h1>
+          </div>
+        </div>
       </div>
-    </div>
-    <h3 className='cfheading'>{label}</h3>
+      <h3 className='cfheading'>{label}</h3>
     </div>
   );
 
